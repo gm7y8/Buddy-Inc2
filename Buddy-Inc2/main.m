0ad7f2e41c094601270e3b9fe8bc9cb9024ff3b8
Vineeth Reddy Kanupuru <Vineeth@Vineeths-MacBook-Air.local>
2015-07-14 13:25:50 -0500

    Initial Commit

---
+++ Buddy-Inc2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Buddy-Inc2
+//
+//  Created by Vineeth Reddy Kanupuru on 7/14/15.
+//  Copyright (c) 2015 Sasidhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Buddy-Inc2Tests/Buddy_Inc2Tests.m
@@ -0,0 +1,40 @@
+//
+//  Buddy_Inc2Tests.m
+//  Buddy-Inc2Tests
+//
+//  Created by Vineeth Reddy Kanupuru on 7/14/15.
+//  Copyright (c) 2015 Sasidhar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Buddy_Inc2Tests : XCTestCase
+
+@end
+
+@implementation Buddy_Inc2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
